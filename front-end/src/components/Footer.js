c59c9d7cd2bf345c0d9281096c3a2bb09a96c4bd
teohzy2080 <teohzy2080@gmail.com>
2022-12-09 15:58:53 +0800

    Footer: Add image attributions

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -59,6 +59,17 @@ const Footer = () => {
                                 }
                             </Stack>
                         </Grid>
+                        <Grid item xs={12} sm={5} md={2} pt={3}>
+                            <Stack direction='column' spacing={1}>
+                                <Typography variant='body1' sx={{color: '#fff', fontWeight: 500}}>
+                                    Image attribution
+                                </Typography>
+                                <a style={{color: '#fff', fontWeight: 300, textDecoration: 'none'}} href="https://www.freepik.com/free-vector/food-allergy-abstract-concept-vector-illustration-food-ingredient-intolerance-allergy-treatment-allergen-identification-risk-factor-skin-rash-problem-gluten-free-diet-abstract-metaphor_11666927.htm#query=food%20allergy&position=2&from_view=keyword">Image by vectorjuice</a>
+                                <a style={{color: '#fff', fontWeight: 300, textDecoration: 'none'}} href="https://www.freepik.com/free-vector/tiny-people-choosing-food-ketogenic-diet_9650826.htm#query=nutrition&position=28&from_view=search&track=sph">Image by pch.vector</a>
+	                            <a style={{color: '#fff', fontWeight: 300, textDecoration: 'none'}} href="https://www.freepik.com/free-vector/cashier-concept-worker-cashier-counter-supermarket-shop-store-client-service-payment-operation-cash-accounting-calculations-vector-illustration_26195346.htm#page=3&query=barcode%20scan&position=12&from_view=search&track=sph">Image by vector4stock</a>
+	                            <a style={{color: '#fff', fontWeight: 300, textDecoration: 'none'}} href="https://www.freepik.com/free-vector/man-having-food-allergy-symptoms-products-like-fish-milk-eggs-food-allergy-food-alergen-ingredient-allergy-risk-factor-concept-bright-vibrant-violet-isolated-illustration_10780620.htm#page=3&query=food%20allergy&position=16&from_view=search&track=sph">Image by vectorjuice</a>
+                            </Stack>
+                        </Grid>
                     </Grid>
                 </Container>
             </Box>
