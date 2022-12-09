6de162992cd5ad7fd272cd671dbca772b5e89068
teohzy2080 <teohzy2080@gmail.com>
2022-12-09 13:17:32 +0800

    Error 404 page: add alt text, add error 404 svg

--- front-end/src/pages/PageNotFound.js
+++ front-end/src/pages/PageNotFound.js
@@ -10,7 +10,7 @@ const PageNotFound = () => {
                 <Grid item xs={12} md={8}>
                     <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center'}}>
                         <Typography variant='h3' sx={{fontWeight: 600, mb:1}} color='secondary.dark'>Error 404</Typography>
-                        <img src={error404} sx={{width:'100%', height: 'auto'}} width={250} height={250}/>
+                        <img src={error404} sx={{width:'100%', height: 'auto'}} alt='error 404 page not found' width={250} height={250}/>
                         <Typography variant='h4' sx={{fontWeight: 500, mb:1, mt:2}} color='secondary.dark'>Page Not Found</Typography>
                         <Typography variant='body1' sx={{fontWeight: 400, fontSize: '1.2rem'}}>We're sorry the page that you are looking for could not be found. Please return to the homepage using the button below</Typography>
                         <Button variant='contained' size='large' component={RouterLink} to='/' color="secondary" sx={{mt:3}} endIcon={<HomeIcon />}>Back to home</Button>
