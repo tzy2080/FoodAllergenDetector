c034d4a802e5f8a95c9be864dae83432de024ec0
teohzy2080 <teohzy2080@gmail.com>
2022-12-10 10:34:27 +0800

    Create model that stores the type of allergens

---
+++ back-end/models/allergen_model.js
@@ -0,0 +1,13 @@
+// Import packages
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const allergenSchema = new Schema({
+    allergenId: {type: String, required: true},
+    allergenName: {type: String, required: true}
+})
+
+const Allergen = mongoose.model('Allergen', allergenSchema);
+
+module.exports = { Allergen };
