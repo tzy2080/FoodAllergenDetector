c9a6f32030be26c9aba3ce32d4cdef0bd86df1f2
teohzy2080 <teohzy2080@gmail.com>
2022-12-10 10:33:49 +0800

    Get data from allergen API and store in DB

---
+++ back-end/retrieveAllergen.js
@@ -0,0 +1,32 @@
+const axios  = require('axios');
+const allergenModel = require('./models/allergen_model');
+const allergen = allergenModel.Allergen;
+
+// Update and retrieve allergen list
+const getAllergens = async () => {
+    await axios.get('https://world.openfoodfacts.org/allergens.json')
+        .then(async (res) => {
+            const allergens = res.data.tags;
+            // Filter out allergens that are not in english or none
+            const filteredAllergens = allergens.filter((allergen) => {
+                return allergen.id.slice(0,2) === 'en' && allergen.name !== 'None' && allergen.name !== 'none-specified';
+            })
+            // Delete all records
+            await allergen.deleteMany({})
+                .then(() => {
+                    // Insert filteredAllergens into database
+                    filteredAllergens.forEach(async (allergenItem) => {
+                        await allergen.create([{ allergenId: allergenItem.id , allergenName: allergenItem.name}]);
+                    })
+                    console.log('Allergen list updated');
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
+module.exports = getAllergens;
