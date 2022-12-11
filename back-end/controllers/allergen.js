eadf85ec7a32cdae0a7e03ac1e955c21f4cfc278
teohzy2080 <teohzy2080@gmail.com>
2022-12-11 14:35:11 +0800

    Modify add new allergen function

--- back-end/controllers/allergen.js
+++ back-end/controllers/allergen.js
@@ -10,7 +10,7 @@ const getAllergens = async (req, res) => {
         const userId = req.user;
 
         // Retrieve all allergens
-        await userAllergen.find({userId: userId})
+        await userAllergen.find({userId: userId}, '_id allergenId allergenName')
             .then(allergens => res.json(allergens));
     } catch (error) {
         console.log(error);
@@ -22,20 +22,26 @@ const getAllergens = async (req, res) => {
 const addAllergen = async (req, res) => {
     try {
         const userId = req.user;
-        const allergenId = req.body.allergenId;
-        const allergenName = req.body.allergenName;
+        const allergens = req.body.allergens;
 
-        // Add new allergen
-        const newAllergen = new userAllergen({
-            userId,
-            allergenId,
-            allergenName
+        allergens.forEach(async item => {
+            // Check if allergen already exists in the user allergen database
+            await userAllergen.findOne({allergenId: item.allergenId})
+                .then(async (record) => {
+                    // If there is no similar allergen found
+                    if (!record) {
+                        // Check if the allergen is a valid type
+                        await allergen.findOne({allergenId: item.allergenId})
+                        .then((result) => {
+                            // If there is such allergen proceed to insert
+                            if (result) {
+                                userAllergen.create({userId: userId, allergenId: item.allergenId, allergenName: item.allergenName})
+                            }
+                        })
+                    }
+                })
         });
-
-        await newAllergen.save()
-            .then(() => {
-               res.status(200).send(); 
-            });
+        res.status(200).send('success');
     } catch (error) {
         console.log(error);
         res.status(500).send();
@@ -53,7 +59,6 @@ const deleteAllergen = async (req, res) => {
             .then(async allergen => {
                 // If the allergen does not belong to the user send error
                 if (String(allergen.userId) !== userId){
-                    console.log(error);
                     res.status(500).send();
                 }
                 // If the allergen belongs to the user then proceed to delete
